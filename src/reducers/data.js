5636bfb65eb10866aebdb4bf5c2cfc3eacf764fd
Noel Vaca (Ubuntu) <noel.vm.2501@gmail.com>
2018-01-29 09:29:17 -0400

    Actualizando un estado inmutable

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -4,34 +4,13 @@ import { fromJS } from 'immutable';
 const initialState = fromJS({
   entities: schema.entities,
   categories: schema.result.categories,
-  search: [],
+  search: '',
 })
 
 function data(state = initialState, action) {
     switch (action.type) {
         case 'SEARCH_VIDEO': {
-            //action.payload.query
-            //const list = state.data.categories[2].playlist
-            //const results = list.filter((item) => {
-            //    return item.author.includes(action.payload.query)
-            //})
-            let results = []
-            if(action.payload.query){
-              state.data.categories.map(
-                category => {
-                   results = results.concat(
-                     category.playlist.filter(
-                       item => item.author.toLowerCase()
-                          .includes( action.payload.query.toLowerCase() )
-                     )
-                   )
-                 }
-               )
-            }
-            return {
-                ...state, 
-                search: results
-            }
+            return state.set('search', action.payload.query)
         }
         default:
             return state
